755e3d43b64d808d93b24978dea05b33e528c3d5
Abdullah <ma0401431@gmail.com>
2023-02-25 10:52:27 -0800

    fixed the api basename problem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
   reducerPath: "Api",
   baseQuery: fetchBaseQuery({
-    baseUrl:'localhost:5700/api',
+    baseUrl:'http://localhost:5700/api',
     prepareHeaders: (headers) => {
 
       headers.set("Content-Type", "application/json")
