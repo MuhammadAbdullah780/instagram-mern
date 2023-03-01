d1cd097911e7d4a77f109ce6cf2a3da09f76ecb0
Abdullah <ma0401431@gmail.com>
2023-03-01 06:34:37 -0800

    -

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
   reducerPath: "Api",
   baseQuery: fetchBaseQuery({
-    baseUrl:'http://localhost:5700/api',
+    baseUrl:'/api',
     prepareHeaders: (headers) => {
 
       headers.set("Content-Type", "application/json")
